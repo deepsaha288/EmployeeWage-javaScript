680fe24e4dd1307b97e39fc01ddae47c5779710c
Deep Saha <deepsaha288@yahoo.com>
2021-04-27 00:14:40 +0530

    added employee part time wage

--- employeepayroll.js
+++ employeepayroll.js
@@ -1,19 +1,3 @@
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HOURS=4;
-const FULL_TIME_HOURS=8;
-const WAGE_PER_HOUR=20;
-let empHrs=4;
-empCheck =Math.floor(Math.random()*10)%3;
-switch(empCheck){
-    case IS_PART_TIME:
-        empCheck=PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empCheck=FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs=4;
-}
-let empWage=empHrs*WAGE_PER_HOUR;
-console.log("Employee Wage:"+empWage);
+const Utility = require('./Utility');
+const result = Utility.empWage();
+console.log(result);
