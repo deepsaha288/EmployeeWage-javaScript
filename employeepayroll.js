84f09e52f133e1e541c48459a5d8e9c7837a00d4
Deep Saha <deepsaha288@yahoo.com>
2021-04-28 23:36:16 +0530

    added daily wage along with the total wage

--- employeepayroll.js
+++ employeepayroll.js
@@ -1,3 +1,37 @@
-const Utility = require('./Utility');
-const totalSalary = Utility.computeEmpWage();;
-console.log(`Total salary = ${totalSalary}`);
+const Num_OF_WORKING_DAYS = 20;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2
+const PART_TIME_HOURS = 4;
+const FUL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const MAX_HRS_IN_MONTH = 160;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empHrs = 0;
+let empDailyWageArr = new Array();
+
+function calDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FUL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < Num_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calDailyWage(empHrs));
+}
+
+let empWage = calDailyWage(totalEmpHrs);
+console.log("Total Days " + totalWorkingDays + " Total Hrs " + totalEmpHrs + " Emp Wage " + empWage);
